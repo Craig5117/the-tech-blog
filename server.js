e921db62a97a7245fb3c7e4abd004f017e54d50e
Craig5117 <craig.bennett5117@gmail.com>
2021-01-22 21:30:18 -0500

    added main, homepage and set up handlebars engine

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const sequelize = require("./config/connection");
 const express = require('express');
 const routes = require('./controllers');
-// const path = require('path');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({})
 
 
 const app = express();
 
 const PORT = process.env.PORT || 3001
-// handlebars engine
-// handlebars set 
+app.engine('handlebars', hbs.engine);
+// alternately like this:
+// app.engine('handlebars', exphbs({ can set defaultLayout and helpers here}));
+app.set('view engine', 'handlebars');
 
 // use session
 app.use(express.json());
