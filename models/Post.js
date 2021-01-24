b35e19fc822a2f4937408cf92d7e421ce475a11b
Craig5117 <craig.bennett5117@gmail.com>
2021-01-24 15:13:42 -0500

    new post is added but displays without returns

--- models/Post.js
+++ models/Post.js
@@ -16,7 +16,7 @@ Post.init(
       allowNull: false,
     },
     post_body: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false,
         validate: {
             len: [1, 15000]
--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.set("view engine", "handlebars");
 app.use(session(sess));
 // middleware for post requests
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 // use static files in public
 app.use(express.static(path.join(__dirname, "public")));
 
