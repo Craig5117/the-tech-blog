cef1fd4105cfcbbac608d01b90efd6be3573eada
Craig5117 <craig.bennett5117@gmail.com>
2021-01-24 00:12:21 -0500

    connected login to session and added logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -66,7 +66,13 @@ router.post("/", (req, res) => {
   })
     .then((dbUserData) => {
       // session code here
-      res.json(dbUserData);
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -80,28 +86,45 @@ router.post("/login", (req, res) => {
     where: {
       username: req.body.username,
     },
-  })
-    .then((dbUserData) => {
-      if (!dbUserData) {
-        res.status(400).json({ message: "No user found with that username." });
-        return;
-      }
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user found with that username." });
+      return;
+    }
 
-      // user verification
-      const validPassword = dbUserData.checkPassword(req.body.password);
-      if (!validPassword) {
-        res.status(400).json({ message: "Password does not match with username." });
-        return;
-      }
+    // user verification
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Password does not match with username." });
+      return;
+    }
+    req.session.save(() => {
+      // sets session variables to confirm user is logged in
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
       res.json({ user: dbUserData, message: "You are now logged in!" });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
     });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // logout
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 // Update and Delete Logic would go here
 
--- server.js
+++ server.js
@@ -1,27 +1,43 @@
 const sequelize = require("./config/connection");
-const express = require('express');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers.js')
-const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers })
+const express = require("express");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers.js");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+const sess = {
+  secret: "aywtd?inshfy?283",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  // stores the new session to the database exported in config/connection
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 const app = express();
 
-const PORT = process.env.PORT || 3001
-app.engine('handlebars', hbs.engine);
+const PORT = process.env.PORT || 3001;
+app.engine("handlebars", hbs.engine);
 // alternately like this:
 // app.engine('handlebars', exphbs({ can set defaultLayout and helpers here}));
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 
 // use session
+app.use(session(sess));
+// middleware for post requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+// use static files in public
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
+// initialize connection to the database then start the server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
